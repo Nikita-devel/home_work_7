5e42e704b6ed03d1477e9179ced8c2fd855be34d
Chumak Nikita <brseve90@gmail.com>
2023-05-24 21:41:55 +0200

    sort

--- clean_folder/clean_folder/clean.py
+++ clean_folder/clean_folder/clean.py
@@ -1,16 +1,83 @@
 import os
 import shutil
+import sys
 
+def normalize(filename):
+    translit_table = {
+        'а': 'a', 'б': 'b', 'в': 'v', 'г': 'h', 'ґ': 'g', 'д': 'd', 'е': 'e', 'є': 'ie',
+        'ж': 'zh', 'з': 'z', 'и': 'y', 'і': 'i', 'ї': 'i', 'й': 'i', 'к': 'k', 'л': 'l',
+        'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 'у': 'u',
+        'ф': 'f', 'х': 'kh', 'ц': 'ts', 'ч': 'ch', 'ш': 'sh', 'щ': 'shch', 'ю': 'iu',
+        'я': 'ia',
+        'А': 'A', 'Б': 'B', 'В': 'V', 'Г': 'H', 'Ґ': 'G', 'Д': 'D', 'Е': 'E', 'Є': 'Ye',
+        'Ж': 'Zh', 'З': 'Z', 'И': 'Y', 'І': 'I', 'Ї': 'Yi', 'Й': 'Y', 'К': 'K', 'Л': 'L',
+        'М': 'M', 'Н': 'N', 'О': 'O', 'П': 'P', 'Р': 'R', 'С': 'S', 'Т': 'T', 'У': 'U',
+        'Ф': 'F', 'Х': 'Kh', 'Ц': 'Ts', 'Ч': 'Ch', 'Ш': 'Sh', 'Щ': 'Shch', 'Ю': 'Yu',
+        'Я': 'Ya'
+    }
 
-def clean_folder(path):
-    for filename in os.listdir(path):
-        file_path = os.path.join(path, filename)
-        if os.path.isfile(file_path):
-            os.remove(file_path)
-        elif os.path.isdir(file_path):
-            shutil.rmtree(file_path)
+    result = ''
+    for char in filename:
+        if char.isalnum():
+            result += char
+        elif char in translit_table:
+            result += translit_table[char]
+        else:
+            result += '_'
+    return result
 
+def process_folder(folder_path):
+    known_extensions = {
+        'images': ('JPEG', 'PNG', 'JPG', 'SVG'),
+        'video': ('AVI', 'MP4', 'MOV', 'MKV'),
+        'documents': ('DOC', 'DOCX', 'TXT', 'PDF', 'XLSX', 'PPTX'),
+        'audio': ('MP3', 'OGG', 'WAV', 'AMR'),
+        'archives': ('ZIP', 'GZ', 'TAR')
+    }
+    unknown_extensions = set()
 
-if __name__ == "__main__":
-    folder_path = input("Enter the folder path: ")
-    clean_folder(folder_path)
+    for root, dirs, files in os.walk(folder_path):
+        for filename in files:
+            file_path = os.path.join(root, filename)
+            file_extension = filename.split('.')[-1].upper()
+
+            if file_extension in known_extensions['images']:
+                destination = 'images'
+            elif file_extension in known_extensions['video']:
+                destination = 'video'
+            elif file_extension in known_extensions['documents']:
+                destination = 'documents'
+            elif file_extension in known_extensions['audio']:
+                destination = 'audio'
+            elif file_extension in known_extensions['archives']:
+                destination = 'archives'
+                unpacked_folder = os.path.splitext(file_path)[0]
+                shutil.unpack_archive(file_path, unpacked_folder)
+                process_folder(unpacked_folder)
+            else:
+                destination = 'unknown'
+                unknown_extensions.add(file_extension)
+
+            if destination != 'unknown':
+                destination_folder = os.path.join(folder_path, destination)
+                os.makedirs(destination_folder, exist_ok=True)
+                new_filename = normalize(filename)
+                new_file_path = os.path.join(destination_folder, new_filename)
+                shutil.move(file_path, new_file_path)
+
+    return known_extensions, unknown_extensions
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Please provide the folder path as an argument.")
+    else:
+        folder_path = sys.argv[1]
+        known_extensions, unknown_extensions = process_folder(folder_path)
+
+        print("Known extensions:")
+        for category, extensions in known_extensions.items():
+            print(f"{category}: {', '.join(extensions)}")
+
+        print("\nUnknown extensions:")
+        print(', '.join(unknown_extensions))
+# test
--- clean_folder/setup.py
+++ clean_folder/setup.py
@@ -1,13 +1,13 @@
-from setuptools import setup
-
+from setuptools import setup, find_namespace_packages
 
 setup(
-    name="clean_folder",
-    version="1.0",
-    packages=["clean_folder"],
-    entry_points={
-        "console_scripts": [
-            "clean-folder=clean_folder.clean:main",
-        ],
-    },
+    name='clean_folder',
+    version='0.1',
+    description='Sorting files be category',
+    url='https://github.com/Nikita-devel/home_work_7',
+    author='Chumak Nikita',
+    author_email='brseven90@gmail.com',
+    license='UA',
+    packages=find_namespace_packages(),
+    entry_points={'console_scripts': ['clean-folder = clean_folder.sort:main']}
 )
